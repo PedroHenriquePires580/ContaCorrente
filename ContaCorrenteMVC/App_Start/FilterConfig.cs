63651a75bca9d437dea4b5ef36d101bfe9cc2cbe
Pedro Henrique Pires <pedrohenriquepires2@hotmail.com>
2019-04-12 10:49:52 -0300

    esqueleto basico

---
+++ ContaCorrenteMVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ContaCorrenteMVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
